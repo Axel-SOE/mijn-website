3c499c97a639056fea8cf21d9bbac7db67b2e3fc
Axel-SOE <axelsoebi@gmail.com>
2025-05-18 12:52:57 +0200

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
-var map = L.map('map').setView([51.29, 4.49], 13);
+var map = L.map('map').setView([51.29, 4.49], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.square([
-    [51.26, 4.46],
-    [51.29, 4.45],
-    [51.31, 4.47],
-    [51.30, 4.55]
+L.polygon([
+    [51.30697712504292, 4.546152646361714],
+    [51.34773502086442, 4.500991232727665],
+    [51.29870793748795, 4.446693665725004],
+    [51.27429729775229, 4.500673425161012]
 ],{
     color: '#49C3EB',
     fillOpacity: 0.3
